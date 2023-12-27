5d0a730ca15f6cdd7424e7a5ed24386bb6ed354b
isumitkatkarr <katkarsumit09@gmail.com>
2023-12-27 12:11:04 +0530

    first commit

---
+++ src/Birthday/Data.js
@@ -0,0 +1,24 @@
+const Data = [
+    {
+        "name":"Tiger Shroff",
+        "age":26,
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs9WWOfWlmCaYVYbvAo23xXNDfak89-OgNEXNBVUg7kP8BdhVk"
+
+    },
+    {
+        "name":"Shraddha Kapoor",
+        "age":29,
+        "image":"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSxb85TPLCV569xH1NHPwGBXj445HZ8wAnLcTRY1mDlr7XE8Hrf"
+    },
+    {
+        "name":"Samantha Ruth",
+        "age":36,
+        "image":"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSRBrXNVBNpI93qXhL84PeYX2oe8l4T9_TP6YJWvwCkAWanKcma"
+    },
+    {
+        "name":"Sai Pallavi",
+        "age":31,
+        "image":"https://st1.bollywoodlife.com/wp-content/uploads/2023/01/sai-pallavi-youthful-film-1024x901-1.jpg"
+    }
+]
+export default Data
